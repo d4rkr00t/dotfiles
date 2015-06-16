96e1afcfde7d391e3a166aa4b220cacbd527e6a8
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-06-16 09:02:55 +0300

    Start working on new way of syncing atom

--- sync.sh
+++ sync.sh
@@ -12,8 +12,3 @@ for file in $DIR/link/*; do
     ln -sfF "$file" "$HOME/"
 done
 unset file
-
-# Copy atom icon
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    cp -f misc/atom.icns /Applications/Atom.app/Contents/Resources/atom.icns
-fi

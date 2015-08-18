c2b30b8686197df4fb235c62af64b5b76c793df8
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-08-18 21:53:11 +0300

    Fix output for movtogif

--- functions/.movtogif.sh
+++ functions/.movtogif.sh
@@ -1,4 +1,5 @@
 function movtogif() {
+    local output="${2:-out.gif}"
     local size="${3:-1024}"
     local rate="${4:-10}"
     local colors="${4:-128}"
@@ -7,10 +8,10 @@ function movtogif() {
         echo ""
         echo "Converts Mov files to gif."
         echo ""
-        echo "Usage: mov2gif source.mov [output.gif size:$size frame_rate:$rate colors:$colors]"
+        echo "Usage: mov2gif source.mov [$output size:$size frame_rate:$rate colors:$colors]"
         echo ""
         return 1
     fi
 
-    time ffmpeg -i $1 -r $rate -f image2pipe -vcodec ppm - | time convert -verbose +dither -layers Optimize -resize "${size}x${size}"\> - gif:- | gifsicle --colors $colors --delay=5 --loop --optimize=3 --multifile -> $2
+    time ffmpeg -i $1 -r $rate -f image2pipe -vcodec ppm - | time convert -verbose +dither -layers Optimize -resize "${size}x${size}"\> - gif:- | gifsicle --colors $colors --delay=5 --loop --optimize=3 --multifile -> $output
 }

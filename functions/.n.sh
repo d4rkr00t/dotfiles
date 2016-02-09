9f521e3b6ca8ac780d301a7670b851066a007953
Stanislav Sysoev <d4rkr00t@gmail.com>
2016-02-09 23:14:12 +0300

    Fix for n command

--- functions/.n.sh
+++ functions/.n.sh
@@ -3,11 +3,13 @@
 # Runs given command using binary in node_modules/.bin of the current project
 # https://github.com/ai/environment
 
-if [ -d $(npm bin) ]; then
-	PROG="$1"
-	shift
-	$(npm bin)/$PROG "$@"
-else
-	echo 'No node_modules in any dir of current path' 1>&2
-	return 1
-fi
+function n() {
+	if [ -d $(npm bin) ]; then
+		PROG="$1"
+		shift
+		$(npm bin)/$PROG "$@"
+	else
+		echo 'No node_modules in any dir of current path' 1>&2
+		return 1
+	fi
+}

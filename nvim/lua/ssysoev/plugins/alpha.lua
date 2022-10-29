3da488fdfa8b940f3d2e082f53e24ef0bb87f9e7
Stanislav Sysoev <d4rkr00t@gmail.com>
2022-10-29 21:25:23 +1100

    nvim: update alpha config, hide global mru

--- nvim/lua/ssysoev/plugins/alpha.lua
+++ nvim/lua/ssysoev/plugins/alpha.lua
@@ -4,4 +4,43 @@ if not alpha_setup then
 	return
 end
 
-alpha.setup(require("alpha.themes.startify").config)
+local theme = require("alpha.themes.startify")
+
+theme.section.header.val = {
+	"                                                    ",
+	" ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+	" ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+	" ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+	" ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+	" ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+	" ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+	"                                                    ",
+}
+
+local config = {
+	layout = {
+		{ type = "padding", val = 1 },
+		theme.section.header,
+		{ type = "padding", val = 2 },
+		theme.section.top_buttons,
+		theme.section.mru_cwd,
+		-- section.mru,
+		{ type = "padding", val = 1 },
+		theme.section.bottom_buttons,
+		theme.section.footer,
+	},
+	opts = {
+		margin = 3,
+		redraw_on_resize = false,
+		setup = function()
+			vim.api.nvim_create_autocmd("DirChanged", {
+				pattern = "*",
+				callback = function()
+					require("alpha").redraw()
+				end,
+			})
+		end,
+	},
+}
+
+alpha.setup(config)

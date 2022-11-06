881edd32d8bde301633e596a0d2341a5a5ab0798
Stanislav Sysoev <d4rkr00t@gmail.com>
2022-11-07 10:41:37 +1100

    nvim: change colour scheme

--- nvim/lua/ssysoev/core/colorscheme.lua
+++ nvim/lua/ssysoev/core/colorscheme.lua
@@ -1,33 +1,33 @@
 local safe_require = require("ssysoev.utils.safe-require")
 
-safe_require({ "oh-lucy" }, function(mods)
-	pcall(vim.cmd, "colorscheme oh-lucy")
-end)
-
--- safe_require({ "catppuccin" }, function(mods)
--- 	local catppuccin = mods.catppuccin
---
--- 	pcall(vim.cmd, "colorscheme catppuccin")
---
--- 	catppuccin.setup({
--- 		dim_inactive = {
--- 			enabled = true,
--- 			shade = "dark",
--- 			percentage = 0.5,
--- 		},
--- 		integrations = {
--- 			cmp = true,
--- 			gitsigns = true,
--- 			nvimtree = true,
--- 			symbols_outline = true,
--- 			telescope = true,
--- 			treesitter = true,
--- 			treesitter_context = true,
--- 			lsp_trouble = true,
--- 			-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
--- 		},
--- 	})
+-- safe_require({ "oh-lucy" }, function(mods)
+-- 	pcall(vim.cmd, "colorscheme oh-lucy")
 -- end)
+
+safe_require({ "catppuccin" }, function(mods)
+	local catppuccin = mods.catppuccin
+
+	pcall(vim.cmd, "colorscheme catppuccin")
+
+	catppuccin.setup({
+		dim_inactive = {
+			enabled = true,
+			shade = "dark",
+			percentage = 0.5,
+		},
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			nvimtree = true,
+			symbols_outline = true,
+			telescope = true,
+			treesitter = true,
+			treesitter_context = true,
+			lsp_trouble = true,
+			-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+		},
+	})
+end)
 
 -- safe_require({ "rose-pine" }, function(mods)
 -- 	local rose = mods["rose-pine"]

b975e975a8202fcf729104bb8a5a69cb8f986a49
Stanislav Sysoev <d4rkr00t@gmail.com>
2022-12-10 16:48:08 +1100

    nvim: navic vscode like icons

--- nvim/lua/ssysoev/plugins/barbecue.lua
+++ nvim/lua/ssysoev/plugins/barbecue.lua
@@ -3,9 +3,39 @@ local safe_require = require("ssysoev.utils.safe-require")
 safe_require({ "barbecue", "nvim-navic" }, function(mods)
 	local barbecue = mods.barbecue
 	local navic = mods["nvim-navic"]
-	pcall(barbecue.setup, {})
+	pcall(barbecue.setup, {
+		show_modified = true,
+	})
 
 	navic.setup({
 		highlight = true,
+		icons = {
+			File = " ",
+			Module = " ",
+			Namespace = " ",
+			Package = " ",
+			Class = " ",
+			Method = " ",
+			Property = " ",
+			Field = " ",
+			Constructor = " ",
+			Enum = " ",
+			Interface = " ",
+			Function = " ",
+			Variable = " ",
+			Constant = " ",
+			String = " ",
+			Number = " ",
+			Boolean = " ",
+			Array = " ",
+			Object = " ",
+			Key = " ",
+			Null = " ",
+			EnumMember = " ",
+			Struct = " ",
+			Event = " ",
+			Operator = " ",
+			TypeParameter = " ",
+		},
 	})
 end)

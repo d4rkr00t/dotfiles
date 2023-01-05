daee037ffbffc216e1895c5f735dee846e5336ef
Stanislav Sysoev <d4rkr00t@gmail.com>
2023-01-05 21:43:51 +1100

    nvim: trigger autocomplete on opt+space

--- nvim/lua/ssysoev/plugins/nvim-cmp.lua
+++ nvim/lua/ssysoev/plugins/nvim-cmp.lua
@@ -47,7 +47,7 @@ safe_require({ "cmp", "luasnip", "lspkind" }, function(mods)
 			["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-\\>"] = cmp.mapping.complete(), -- show completion suggestions
+			["<M- >"] = cmp.mapping.complete(), -- show completion suggestions
 			["<C-e>"] = cmp.mapping.abort(), -- close completion window
 			["<CR>"] = cmp.mapping.confirm({
 				behavior = cmp.ConfirmBehavior.Replace,

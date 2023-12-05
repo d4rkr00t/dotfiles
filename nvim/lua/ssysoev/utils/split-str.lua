d7c1e0e31f3fc794eee9670e73f206ce29f465c6
Stanislav Sysoev <d4rkr00t@gmail.com>
2023-12-05 11:38:12 +1100

    nvim: add matcher for stash urls

---
+++ nvim/lua/ssysoev/utils/split-str.lua
@@ -0,0 +1,13 @@
+function split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  local i = 0
+  for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    table.insert(t, i, str)
+    i = i + 1
+  end
+  return t
+end
+return split

bab7545f4f732dfc61a1b79afa46e452b2c10e18
Stanislav Sysoev <d4rkr00t@gmail.com>
2023-12-28 10:58:44 +1100

    nvim: add stash link matcher

--- nvim/lua/ssysoev/plugins/gitlinker.lua
+++ nvim/lua/ssysoev/plugins/gitlinker.lua
@@ -2,21 +2,25 @@ local safe_require = require("ssysoev.utils.safe-require")
 local split = require("ssysoev.utils.split-str")
 safe_require({ "gitlinker" }, function(mods)
   local gitlinker = mods["gitlinker"]
+
+  local function stash_matcher(url_data)
+    local url = "https://stash.atlassian.com/projects/"
+    local repo = split(url_data.repo, "/")
+    url = url .. repo[0] .. "/repos/" .. repo[1]
+    url = url .. "/browse/" .. url_data.file
+    url = url .. "?at=" .. url_data.rev
+    if url_data.lend then
+      url = url .. "#" .. url_data.lstart .. "-" .. url_data.lend
+    else
+      url = url .. "#" .. url_data.lstart
+    end
+    return url
+  end
+
   gitlinker.setup({
     callbacks = {
-      ["bitbucket-mirror-au.internal.atlassian.com"] = function(url_data)
-        local url = "https://stash.atlassian.com/projects/"
-        local repo = split(url_data.repo, "/")
-        url = url .. repo[0] .. "/repos/" .. repo[1]
-        url = url .. "/browse/" .. url_data.file
-        url = url .. "?at=" .. url_data.rev
-        if url_data.lend then
-          url = url .. "#" .. url_data.lstart .. "-" .. url_data.lend
-        else
-          url = url .. "#" .. url_data.lstart
-        end
-        return url
-      end,
+      ["bitbucket-mirror-au.internal.atlassian.com"] = stash_matcher,
+      ["stash.atlassian.com"] = stash_matcher,
     },
   })
 end)

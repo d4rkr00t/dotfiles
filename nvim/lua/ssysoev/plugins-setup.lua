0e62c8498dc00d0eab19f035107982df1156efbd
Stanislav Sysoev <d4rkr00t@gmail.com>
2024-12-01 12:27:57 +1100

    nvim: unpin fzf-lua

--- nvim/lua/ssysoev/plugins-setup.lua
+++ nvim/lua/ssysoev/plugins-setup.lua
@@ -365,9 +365,7 @@ return {
   {
     "ibhagwan/fzf-lua",
     -- optional for icon support
-    dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = true,
-    commit = "9427dc65afaa7972fb20fc52280cd1449f691928",
     cmd = { "FzfLua" },
     config = function()
       require("fzf-lua").setup({

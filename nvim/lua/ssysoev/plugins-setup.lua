92628d3de55a092f2a6edcfb086324654b36454f
Stanislav Sysoev <d4rkr00t@gmail.com>
2025-09-10 08:36:48 +1000

    nvim: push latest changes

--- nvim/lua/ssysoev/plugins-setup.lua
+++ nvim/lua/ssysoev/plugins-setup.lua
@@ -180,29 +180,30 @@ return {
   {
     "kevinhwang91/nvim-bqf",
     event = "FileType qf",
-    config = function()
-      local bqf_pv_timer
-      require("bqf").setup({
-        preview = {
-          should_preview_cb = function(bufnr, qwinid)
-            local bufname = vim.api.nvim_buf_get_name(bufnr)
-            if bufname:match("^fugitive://") and not vim.api.nvim_buf_is_loaded(bufnr) then
-              if bqf_pv_timer and bqf_pv_timer:get_due_in() > 0 then
-                bqf_pv_timer:stop()
-                bqf_pv_timer = nil
-              end
-              bqf_pv_timer = vim.defer_fn(function()
-                vim.api.nvim_buf_call(bufnr, function()
-                  vim.cmd(("do fugitive BufReadCmd %s"):format(bufname))
-                end)
-                require("bqf.preview.handler").open(qwinid, nil, true)
-              end, 60)
-            end
-            return true
-          end,
-        },
-      })
-    end,
+    -- cond = false,
+    -- config = function()
+    --   local bqf_pv_timer
+    --   require("bqf").setup({
+    --     preview = {
+    --       should_preview_cb = function(bufnr, qwinid)
+    --         local bufname = vim.api.nvim_buf_get_name(bufnr)
+    --         if bufname:match("^fugitive://") and not vim.api.nvim_buf_is_loaded(bufnr) then
+    --           if bqf_pv_timer and bqf_pv_timer:get_due_in() > 0 then
+    --             bqf_pv_timer:stop()
+    --             bqf_pv_timer = nil
+    --           end
+    --           bqf_pv_timer = vim.defer_fn(function()
+    --             vim.api.nvim_buf_call(bufnr, function()
+    --               vim.cmd(("do fugitive BufReadCmd %s"):format(bufname))
+    --             end)
+    --             require("bqf.preview.handler").open(qwinid, nil, true)
+    --           end, 60)
+    --         end
+    --         return true
+    --       end,
+    --     },
+    --   })
+    -- end,
   },
 
   {

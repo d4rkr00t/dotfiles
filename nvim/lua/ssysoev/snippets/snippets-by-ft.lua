7356ac02d81ca53296ea034de44aacfbe62d06c7
Stanislav Sysoev <d4rkr00t@gmail.com>
2025-11-03 20:02:41 +1100

    nvim: add more css snippets

--- nvim/lua/ssysoev/snippets/snippets-by-ft.lua
+++ nvim/lua/ssysoev/snippets/snippets-by-ft.lua
@@ -1,5 +1,5 @@
 local ts_js_common = {
-  { trigger = "cl", body = "console.log(${1});" },
+  { trigger = "cl",  body = "console.log(${1});" },
   { trigger = "clj", body = "console.log(JSON.stringify(${1}, null, 2));" },
   { trigger = "fun", body = "function ${1:name}(${2:args}) { $0 }" },
   { trigger = "rbt", body = "Math.floor(Math.random() * ($0 - $1 + 1)) + $1;" },
@@ -13,17 +13,48 @@ local M = {
     {
       trigger = "!html",
       body = "<!DOCTYPE html>\n"
-        .. '<html lang="en">\n'
-        .. "<head>\n"
-        .. '  <meta charset="UTF-8">\n'
-        .. '  <meta name="viewport" content="width=device-width, initial-scale=1.0">\n'
-        .. "  <title>Document</title>\n"
-        .. "</head>\n"
-        .. "<body>\n"
-        .. "$0\n"
-        .. "</body>\n"
-        .. "</html>",
+          .. '<html lang="en">\n'
+          .. "<head>\n"
+          .. '  <meta charset="UTF-8">\n'
+          .. '  <meta name="viewport" content="width=device-width, initial-scale=1.0">\n'
+          .. "  <title>Document</title>\n"
+          .. "</head>\n"
+          .. "<body>\n"
+          .. "$0\n"
+          .. "</body>\n"
+          .. "</html>",
     },
+  },
+  css = {
+    {
+      trigger = "fz",
+      body = "font-size: $0;"
+    },
+    {
+      trigger = "bg",
+      body = "background: $0;"
+    },
+    {
+      trigger = "bgc",
+      body = "background-color: $0;"
+    },
+    {
+      trigger = "posa",
+      body = "position: absolute;"
+    },
+    {
+      trigger = "posr",
+      body = "position: relative;"
+    },
+    {
+      trigger = "p",
+      body = "padding: $0;"
+    },
+    {
+      trigger = "zi",
+      body = "z-index: $0;"
+    }
+
   },
   javascript = ts_js_common,
   typescript = ts_js_common,

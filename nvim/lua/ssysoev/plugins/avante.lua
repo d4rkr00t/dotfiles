8b9fd660d16803e08c59441063f4b581059ee2c8
Stanislav Sysoev <d4rkr00t@gmail.com>
2025-11-15 15:24:40 +1100

    nvim: update avante settings

--- nvim/lua/ssysoev/plugins/avante.lua
+++ nvim/lua/ssysoev/plugins/avante.lua
@@ -11,21 +11,41 @@ return {
         return "make"
       end
     end,
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    ---@module 'avante'
-    ---@type avante.Config
-    opts = {},
+    keys = { '<leader>aa', '<leader>an' },
     config = function()
-      local atl_avante = require("ssysoev.custom.atl-avante")
-
+      ---@module 'avante'
+      ---@type avante.Config
       require("avante").setup({
-        provider = "atlgemini",
-        providers = vim.tbl_deep_extend("force", {}, atl_avante),
+        provider = "gemini",
+        mode = "legacy",
+        behaviour = {
+          auto_focus_sidebar = true,
+          auto_suggestions = true, -- Experimental stage
+          auto_suggestions_respect_ignore = true,
+          auto_set_highlight_group = true,
+          auto_set_keymaps = true,
+          auto_apply_diff_after_generation = true,
+          jump_result_buffer_on_finish = false,
+          support_paste_from_clipboard = false,
+          minimize_diff = true,
+          enable_token_counting = true,
+          use_cwd_as_project_root = true,
+          auto_focus_on_diff_view = false,
+        },
         windows = {
           width = 40,
         },
       })
+
+      -- local atl_avante = require("ssysoev.custom.atl-avante")
+      --
+      -- require("avante").setup({
+      --   provider = "atlgemini",
+      --   providers = vim.tbl_deep_extend("force", {}, atl_avante),
+      --   windows = {
+      --     width = 40,
+      --   },
+      -- })
     end,
     dependencies = {
       "nvim-lua/plenary.nvim",

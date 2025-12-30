469e60bec4ac568b41dcf4f7ab2aa6034ec00c66
Stanislav Sysoev <d4rkr00t@gmail.com>
2025-12-30 15:26:47 +1100

    nvim: adjust colorsheme for new treesitter

--- nvim/lua/ssysoev/plugins/colorscheme.lua
+++ nvim/lua/ssysoev/plugins/colorscheme.lua
@@ -12,7 +12,7 @@ return {
           theme = {
             dragon = {
               ui = {
-                fg = "#ffffff",
+                fg = "#f5f5f5",
                 fg_dim = "#A0A0A0",
                 fg_reverse = "#505050",
 
@@ -36,7 +36,7 @@ return {
                 bg_search = "#a0a0a0",
 
                 pmenu = {
-                  fg = "#ffffff",
+                  fg = "#f5f5f5",
                   fg_sel = "none",
                   bg = "#101010",
                   bg_sel = "#242525",
@@ -52,25 +52,28 @@ return {
                 },
               },
               syn = {
-                string = "#99FFE4",
-                variable = "#ffffff",
-                number = "#FFC799",
-                constant = "#FFC799",
-                identifier = "#FFC799",
-                parameter = "#A0A0A0",
+                comment = "#515051",
+
+                string = "#6ad0b7",
+                number = "#6ad0b7",
+
+                constant = "#f5f5f5",
+                variable = "#f5f5f5",
+                identifier = "#f5f5f5",
                 fun = "#FFC799",
-                statement = "#FFC799",
-                keyword = "#A0A0A0",
+                parameter = "#f5f5f5",
+                statement = "#A0A0A0",
                 operator = "#A0A0A0",
-                preproc = "#b0b0b0",
-                -- preproc = "#FF8080",
-                type = "#FFC799",
+                keyword = "#A0A0A0",
+                preproc = "#ffffff",
+                type = "#f5f5f5",
+
                 regex = "#A0A0A0",
                 deprecated = "#A0A0A0",
-                punct = "#A0A0A0",
-                comment = "#515051",
-                special1 = "#FFC799",
-                special2 = "#A0A0A0",
+                punct = "#65737E",
+
+                special1 = "#FFCFA8",
+                special2 = "#65737E",
                 special3 = "#A0A0A0",
               },
               diag = {
@@ -91,26 +94,6 @@ return {
                 removed = "#FF8080",
                 changed = "#FFC799",
               },
-              -- term = {
-              --   palette.dragonBlack0, -- black
-              --   palette.dragonRed, -- red
-              --   palette.dragonGreen2, -- green
-              --   palette.dragonYellow, -- yellow
-              --   palette.dragonBlue2, -- blue
-              --   palette.dragonPink, -- magenta
-              --   palette.dragonAqua, -- cyan
-              --   palette.oldWhite, -- white
-              --   palette.dragonGray, -- bright black
-              --   palette.waveRed, -- bright red
-              --   palette.dragonGreen, -- bright green
-              --   palette.carpYellow, -- bright yellow
-              --   palette.springBlue, -- bright blue
-              --   palette.springViolet1, -- bright magenta
-              --   palette.waveAqua2, -- bright cyan
-              --   palette.dragonWhite, -- bright white
-              --   palette.dragonOrange, -- extended color 1
-              --   palette.dragonOrange2, -- extended color 2
-              -- },
             },
           },
         },
@@ -118,9 +101,16 @@ return {
           dark = "dragon",
         },
         overrides = function(colors)
-          return {}
+          return {
+            Boolean   = { fg = "#6ad0b7" },
+            Property  = { fg = "#6ad0b7" },
+            Field     = { fg = "#6ad0b7" },
+            Operator  = { fg = "#A0A0A0" },
+            Exception = { fg = "#FFCFA8" },
+          }
         end,
       })
+
       pcall(vim.cmd, "colorscheme kanagawa-dragon")
     end,
   },

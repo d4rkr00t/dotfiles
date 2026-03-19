2da36c47da2d217be474d886c8dfce2c091c7758
Stanislav Sysoev <d4rkr00t@gmail.com>
2026-03-19 12:05:14 +1100

    nvim: make sure treesitter grammars are installed

--- nvim/lua/ssysoev/plugins/nvim-treesitter.lua
+++ nvim/lua/ssysoev/plugins/nvim-treesitter.lua
@@ -3,6 +3,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     lazy = false,
-    build = ':TSUpdate'
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter').install { "vim", "markdown", "lua", "jsx", "tsx", "typescript", "rust", "go", "python", "zig", "json", "json5" }
+    end
   },
 }

4af3b30782ef432d806a402ef2305fc93d697a5e
Stanislav Sysoev <d4rkr00t@gmail.com>
2026-04-01 17:00:54 +1100

    nvim: lazy load treesitter

--- nvim/lua/ssysoev/plugins/nvim-treesitter.lua
+++ nvim/lua/ssysoev/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
-    lazy = false,
+    event = { "BufReadPost", "BufNewFile" },
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter').install { "vim", "markdown", "lua", "jsx", "tsx", "typescript", "rust", "go", "python", "zig", "json", "json5", "regex" }

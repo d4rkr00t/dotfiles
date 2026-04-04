fbffc50d231f893c292cf13a1dc216d76271ff0c
Stanislav Sysoev <d4rkr00t@gmail.com>
2026-04-04 11:25:06 +1100

    nvim: fix markdown tag highlight

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,10 @@
 vim.cmd("setlocal wrap")
 
-vim.api.nvim_set_hl(0, "MarkdownTag", { link = "DiagnosticError" })
-vim.fn.matchadd("MarkdownTag", [[#\([a-zA-Z0-9_-]\+\)\+]])
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("markdown_tag_highlight", { clear = true }),
+  callback = function()
+    vim.api.nvim_set_hl(0, "MarkdownTag", { link = "DiagnosticError" })
+    vim.fn.matchadd("MarkdownTag", [[#\([a-zA-Z0-9_-]\+\)\+]])
+  end,
+})
